afc502e28598350e3604cb990266edc8a8b3202a
rzninvo <rzninvo@gmail.com>
2023-05-17 06:12:02 -0700

    Added Turtlebot3 Packages

---
+++ catkin_ws/devel/share/turtlebot3_msgs/cmake/turtlebot3_msgs-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(turtlebot3_msgs_MESSAGE_FILES "/home/rohamzn/Robotics-Course/catkin_ws/src/turtlebot3_msgs/msg/SensorState.msg;/home/rohamzn/Robotics-Course/catkin_ws/src/turtlebot3_msgs/msg/VersionInfo.msg;/home/rohamzn/Robotics-Course/catkin_ws/src/turtlebot3_msgs/msg/Sound.msg")
+set(turtlebot3_msgs_SERVICE_FILES "")
